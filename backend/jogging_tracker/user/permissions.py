8189b9bc3a989765d7d41155fed0d4558343abe5
k1master <jin11uno@gmail.com>
2017-09-15 01:41:38 -0300

    Add permission classes for api endpoints

---
+++ backend/jogging_tracker/user/permissions.py
@@ -0,0 +1,30 @@
+from rest_framework import permissions
+
+from .models import User
+
+__all__ = ['IsUser', 'IsManager', 'IsAdmin']
+
+
+class IsUser(permissions.BasePermission):
+    def has_permission(self, request, view):
+        print(request.user.role)
+        return request.user.is_authenticated() and request.user.role == 'user'
+
+
+class IsManager(permissions.BasePermission):
+    def has_permission(self, request, view):
+        print(request.user.role)
+        return request.user.is_authenticated() and request.user.role == 'manager'
+
+
+class IsAdmin(permissions.BasePermission):
+    def has_permission(self, request, view):
+        print(request.user.role)
+        return request.user.is_authenticated() and request.user.role == 'admin'
+
+class IsAdminOrManager(permissions.BasePermission):
+    def has_permission(self, request, view):
+        print(request.user.role)
+        return request.user.is_authenticated() and (
+            request.user.role == 'admin' or request.user.role == 'manager'
+        )
--- backend/jogging_tracker/user/views.py
+++ backend/jogging_tracker/user/views.py
@@ -1,6 +1,8 @@
 from .models import User
 from .serializers import UserSerializer
 from rest_framework import viewsets
+from rest_framework.decorators import detail_route, list_route
+from .permissions import IsAdminOrManager
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -9,3 +11,4 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
+    permission_classes = [IsAdminOrManager,]
