5aa085fcf6e25638664ed8bd484b113c181f1e06
k1master <jin11uno@gmail.com>
2017-09-23 22:31:05 +0900

    Fix console log

--- frontend/jogging_tracker/src/redux/sagas/index.js
+++ frontend/jogging_tracker/src/redux/sagas/index.js
@@ -1,11 +1,12 @@
+import { all } from 'redux-saga/effects'
 import auth from './auth'
 import tracking from './tracking'
 import user from './user'
 
 export default function* rootSaga () {
-  yield [
+  yield all([
     auth(),
     tracking(),
     user()
-  ]
+  ])
 }
